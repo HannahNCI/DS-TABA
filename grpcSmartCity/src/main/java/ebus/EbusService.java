fc80163f6a016d32f183a6b828d91cd4431eb087
HannahNCI <44295285+HannahNCI@users.noreply.github.com>
2022-12-06 14:52:01 +0100

    Implementing GRPC Service
    Setting up the login / logout user service

---
+++ grpcSmartCity/src/main/java/ebus/EbusService.java
@@ -0,0 +1,16 @@
+package ebus;
+
+import com.smartcityDS.Timetable;
+import com.smartcityDS.busNumber;
+import com.smartcityDS.ebusGrpc.ebusImplBase;
+
+import io.grpc.stub.StreamObserver;
+
+public class EbusService extends ebusImplBase {
+
+	@Override
+	public void getTimetable(busNumber request, StreamObserver<Timetable> responseObserver) {
+	
+	}
+
+}
