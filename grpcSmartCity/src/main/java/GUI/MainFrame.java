0952e73781d60a04733fce80795b132d79492084
HannahNCI <44295285+HannahNCI@users.noreply.github.com>
2022-12-17 15:55:17 +0000

    GUI First Draft

--- grpcSmartCity/src/main/java/GUI/MainFrame.java
+++ grpcSmartCity/src/main/java/GUI/MainFrame.java
@@ -7,6 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.Panel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 
@@ -33,14 +41,36 @@ public class MainFrame extends JFrame {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 745, 525);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("GRPC SMART CITY APPLICATION");
+		JLabel lblNewLabel = new JLabel("SMART CITY APP");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(293, 11, 128, 29);
 		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("ECar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(82, 261, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("EScooter");
+		btnNewButton_1.setBounds(315, 261, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("EBus");
+		btnNewButton_2.setBounds(538, 261, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(140, 64, 456, 141);
+		contentPane.add(textArea);
 	}
-
 }
